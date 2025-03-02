923ff5bb9ada8090ff1565a5fe6f033615fdc7c5
Rajnish Kumar <rajnishkumar8595@gmail.com>
2025-03-02 17:27:07 +0530

    do some final changes

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -2,12 +2,10 @@ require("dotenv").config();
 
 const dbConfig = {
   development: {
-    username: process.env.DB_USERNAME || "postgres",
-    password: process.env.DB_PASSWORD || "postgres123",
-    database: process.env.DB_DATABASE || "todo_app",
-    host:
-      process.env.DB_HOST ||
-      "database-2.cezsmmgawy0x.us-east-1.rds.amazonaws.com",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: "postgres",
     logging: false,
     dialectOptions: {
